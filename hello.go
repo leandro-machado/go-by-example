4a25dc761cc130af4d921b95b72733378159fc45
Leandro Machado <lemachadosantos18@gmail.com>
2020-12-08 15:53:14 -0300

    feat: const in go

--- hello.go
+++ hello.go
@@ -4,19 +4,8 @@ import (
 	"fmt"
 )
 
+const s = "string-const-example"
+
 func main() {
-	var a = "initial"
-	fmt.Println(a, "string variable")
-
-	var b = 1
-	fmt.Println(b, "int variable")
-
-	var c, d int = 100, 150
-	fmt.Println(c, d, "multiple variables")
-
-	var e int
-	fmt.Println(e, "declaring var type is int but not have value, the default value is zero")
-
-	name := "leandro machado"
-	fmt.Println(name, ":= is a shortand to declaring and initialization a var")
+	fmt.Println(s, "string const example")
 }

a44b5bf295746e877b7f01be244867340388f829
Leandro Machado <lemachadosantos18@gmail.com>
2020-12-08 15:39:51 -0300

    feat: variables in go

--- hello.go
+++ hello.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println(quote.Hello())
+	var a = "initial"
+	fmt.Println(a, "string variable")
+
+	var b = 1
+	fmt.Println(b, "int variable")
+
+	var c, d int = 100, 150
+	fmt.Println(c, d, "multiple variables")
+
+	var e int
+	fmt.Println(e, "declaring var type is int but not have value, the default value is zero")
+
+	name := "leandro machado"
+	fmt.Println(name, ":= is a shortand to declaring and initialization a var")
 }
